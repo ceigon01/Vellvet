d0b52505414b8ac1ee67b9887e4c48c9d7a0dfd2
ceigon01 <jsmith@ceigon.com>
2023-01-27 18:58:08 -0700

    added recorder and designer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DesignerComponent } from './designer/designer.component';
+import { RecorderComponent } from './recorder/recorder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignerComponent,
+    RecorderComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +17,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
