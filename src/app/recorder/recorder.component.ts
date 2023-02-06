29be1659d01e142ce9e0475c5be5680a5a852655
ceigon01 <jsmith@ceigon.com>
2023-02-05 23:08:53 -0700

    Added functionality that records audio and sends to server as blob

--- src/app/recorder/recorder.component.ts
+++ src/app/recorder/recorder.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-recorder',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./recorder.component.css']
 })
 export class RecorderComponent {
+  public isRecording:boolean;
+  public recorder:MediaRecorder;
+  public elapsedTime: number;
+
+  getElapsedTime(start: Date): number {
+    const now = new Date();
+    console.log(`Elapsed time: ${this.elapsedTime} milliseconds`);
+    return Math.round((now.getTime() - start.getTime()) / 1000);
+  }
+
+  recordAudio () {
+    if(this.isRecording) {
+      this.recorder.stop();
+      this.isRecording = false;
+      return;
+    }
+    // Get user media
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        let ng = this;
+        // Create a recorder
+        console.log(stream)
+        this.recorder = new MediaRecorder(stream);
+        // Start recording
+        this.recorder.start();
+        let refreshIntervalId
+        this.recorder.onstart = function (ev){
+          let start = new Date();
+          refreshIntervalId = setInterval(() => {
+            // Your task code
+            ng.elapsedTime = ng.getElapsedTime(start);
+          }, 1000);
+
+        }
+
+        this.recorder.onstop = function(ev){
+          console.log("stopped");
+          clearInterval(refreshIntervalId);
+          ng.isRecording = false;
+        }
+        this.recorder.addEventListener('dataavailable', (e) => {
+          // convert the stream to a blob
+          const blob = new Blob([e.data], {type: 'audio/ogg'});
+          console.log(blob);
+        });
+
+        this.isRecording = !this.isRecording;
+      })
+      .catch(err => 'Error getting user media');
+  }
+
+
 
 }
